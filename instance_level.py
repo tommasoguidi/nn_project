c8ceac5e38b88fc57ea18f24b6e593f7a4b1ed24
tommasoguidi <tommaso.guidi.1998@gmail.com>
2023-07-07 19:34:45 +0200

    fix

--- instance_level.py
+++ instance_level.py
@@ -247,11 +247,7 @@ class MoE(nn.Module):
             item_outputs = F.softmax(item_logits)  # class probabilities
             all_item_outputs += (item_outputs,)
 
-        item_logits = torch.vstack(all_item_logits)
-        print(item_logits.size())
-        item_outputs = torch.vstack(all_item_outputs)
-
-        return super_class_logits, super_class_outputs, item_logits, item_outputs
+        return super_class_logits, super_class_outputs, all_item_logits, all_item_outputs
 
 
 class Classifier:
